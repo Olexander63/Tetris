ccad801350eb609a26e5d5829ccd8a90652b7089
Oleksandr Omelchenko <olexander63@gmail.com>
2022-02-17 20:36:15 +0200

    Tetris.sln ,constructor

---
+++ Tetris/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tetris
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+
+            Point p1 = new Point(2, 3, '*');
+            p1.Draw();
+
+
+            Point p2 = new Point()
+            {
+                x = 4,
+                y = 5,
+                c = '#'
+            };
+            p2.Draw();
+
+
+            Console.ReadLine();
+        }
+    }
+}
